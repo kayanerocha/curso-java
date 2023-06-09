a23bb1ad66bbdc60852da9c42f43e98ea5b69203
kayanerocha <kayannerch@gmail.com>
2023-06-09 20:43:15 -0300

    imprimindo a soma dos números digitados

--- EstruturasRepetitivas/src/aulas/Aula2.java
+++ EstruturasRepetitivas/src/aulas/Aula2.java
@@ -12,10 +12,16 @@ public class Aula2 {
 		 */
 		
 		Scanner sc = new Scanner(System.in);
+		
 		int numero = sc.nextInt();
+		
+		int soma = 0;
 		while (numero != 0) {
+			soma += numero;
 			numero = sc.nextInt();
 		}
+		
+		System.out.println(soma);
 		
 		sc.close();
 	}
