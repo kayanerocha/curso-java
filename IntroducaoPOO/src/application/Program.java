bf2bced97bc09481a6cb598748558b28cb52a793
kayanerocha <kayannerch@gmail.com>
2023-06-16 13:40:53 -0300

    metodo que calcula area do triangulo

--- IntroducaoPOO/src/application/Program.java
+++ IntroducaoPOO/src/application/Program.java
@@ -27,11 +27,8 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2;
-		double xArea = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2;
-		double yArea = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double xArea = x.area();
+		double yArea = y.area();
 		
 		System.out.printf("Triangle X area: %.4f\n", xArea);
 		System.out.printf("Triangle Y area: %.4f\n", yArea);
