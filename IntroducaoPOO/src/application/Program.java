a3589f30528e46d5412f4205576e2e8ee8e10960
kayanerocha <kayannerch@gmail.com>
2023-06-16 13:33:05 -0300

    criando classe para triângulo e instanciando no programa principal

--- IntroducaoPOO/src/application/Program.java
+++ IntroducaoPOO/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 	
 	// Aula: Resolvendo um problema sem orientação a objetos
@@ -10,23 +12,26 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangule X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Enter the measures of triangule Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2;
-		double xArea = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2;
+		double xArea = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = (yA + yB + yC) / 2;
-		double yArea = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.a + y.b + y.c) / 2;
+		double yArea = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		
 		System.out.printf("Triangle X area: %.4f\n", xArea);
 		System.out.printf("Triangle Y area: %.4f\n", yArea);
