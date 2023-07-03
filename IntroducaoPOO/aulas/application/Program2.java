55aad1bc3738f262c82f1e290de13019a9e07919
kayanerocha <kayannerch@gmail.com>
2023-07-03 20:02:36 -0300

    utilizando membros estáticos da classe Calculator

--- IntroducaoPOO/aulas/application/Program2.java
+++ IntroducaoPOO/aulas/application/Program2.java
@@ -7,24 +7,26 @@ import util.Calculator;
 
 public class Program2 {
 	
-	// Versão 2: Utilizando membros de instância 
+	/*
+	 * Aula: Membros estáticos
+	 * Versão 3: Utilizando membros estáticos da classe Calculator
+	 */
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
 		
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = calc.circumference(radius);
+		double c = Calculator.circumference(radius);
 		
-		double v = calc.volume(radius);
+		double v = Calculator.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 		
 		sc.close();
 
--- IntroducaoPOO/aulas/util/Calculator.java
+++ IntroducaoPOO/aulas/util/Calculator.java
@@ -2,15 +2,17 @@ package util;
 
 public class Calculator {
 	
-	// final indica que é constante e não muda
-	public final double PI = 3.14159;
+	/*
+	 *  final indica que é constante e não muda
+	 *  é o mesmo valor indenpendete de objeto Calculator
+	 */
+	public static final double PI = 3.14159;
 	
-	// Precisa ser estático, pois o main não permite chamar um método que não é estático
-	public double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2.0 * PI * radius;
 	}
 	
-	public double volume(double radius) {
+	public static double volume(double radius) {
 		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
