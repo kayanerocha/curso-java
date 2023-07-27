1ec83c5f7ea2c663575cf273d42ff6194a6e9b8c
kayanerocha <kayannerch@gmail.com>
2023-07-26 22:39:39 -0300

    exercicio sobre polimorfismo

--- Secao14/exerc1/application/Exerc1.java
+++ Secao14/exerc1/application/Exerc1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import entities.Employee;
 import entities.OutsourcedEmployee;
 
-public class Program {
+public class Exerc1 {
 
 	public static void main(String[] args) {
 		
