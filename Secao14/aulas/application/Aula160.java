d98b838eff24529923cb5ca59ae03fc274595a68
kayanerocha <kayannerch@gmail.com>
2023-07-28 22:55:37 -0300

    comentando instanciação de classe abstrata

--- Secao14/aulas/application/Aula160.java
+++ Secao14/aulas/application/Aula160.java
@@ -8,14 +8,14 @@ public class Aula160 {
 	// Aula: Introdução ao polimorfismo
 	public static void main(String[] args) {
 		
-		Account x = new Account(1020, "Alex", 1000.00);
+		// Account x = new Account(1020, "Alex", 1000.00);
 		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 		
 		// Chama a mesma operação em variáveis do mesmo tipo, porém com comportamentos diferentes de acordo com os objetos apontados
-		x.withdraw(50.0);
+		// x.withdraw(50.0);
 		y.withdraw(50.0);
 		
-		System.out.println(x.getBalance());
+		// System.out.println(x.getBalance());
 		System.out.println(y.getBalance());
 
 	}
