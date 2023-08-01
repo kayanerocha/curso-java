f185debc810436252285ff05ae7fe1276bf0481f
kayanerocha <kayannerch@gmail.com>
2023-08-01 20:30:17 -0300

    solução ruim colocando a validação dos dados na classe do objeto

--- Secao15/exerc1/application/Exerc1.java
+++ Secao15/exerc1/application/Exerc1.java
@@ -41,14 +41,11 @@ public class Exerc1 {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.nextLine());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after checkin date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);			
 				System.out.print("Reservation: " + reservation.toString());
 			}
 		}
