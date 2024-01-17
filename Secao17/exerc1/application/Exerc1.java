229006fa34d89e2cb4e174dc9712a980f333a852
kayanerocha <kayannerch@gmail.com>
2024-01-17 20:21:46 -0300

    lendo e escrevendo em arquivo .csv

---
+++ Secao17/exerc1/application/Exerc1.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exerc1 {
+	
+	public static String getTotal(String line) {
+		ArrayList<String> produto = new ArrayList<>();
+		String listProduto[] = line.split(",");
+		String nome = listProduto[0];
+		double preco = Double.parseDouble(listProduto[1]);
+		int quantidade = Integer.parseInt(listProduto[2]);
+		double total = preco * quantidade;
+		
+		return nome + "," + String.format("%.2f", total).replace(",", ".");
+	}
+	
+	public static void writeFile(String produto) {
+		String strPathOutput = "c:\\Temp\\";
+		new File(strPathOutput + "\\out").mkdir();
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(strPathOutput+"\\out\\summary.csv", true))) {
+			bw.write(produto);
+			bw.newLine();
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Caminho: ");
+		String strPath = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(strPath))) {
+			String line = br.readLine();
+			while (line != null) {
+				String produto = getTotal(line);		
+				line = br.readLine();
+				
+				writeFile(produto);
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
