7ed6a792a5170a659014b110bd80fa98503a49e8
kayanerocha <kayannerch@gmail.com>
2024-01-17 21:02:25 -0300

    conceito de interface

--- Secao17/exerc1/application/Exerc1.java
+++ Secao17/exerc1/application/Exerc1.java
@@ -11,6 +11,13 @@ import java.util.Scanner;
 
 public class Exerc1 {
 	
+	/*
+	 * Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos. Cada item possui um nome, preço unitário e
+	 * quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado "summary.csv", localizado em uma subpasta chamada "out" a partir
+	 * da pasta original do arquivo de origem, contendo apenas o nome e o valor total para aquele item (preço unitário multiplicado pela quantidade),
+	 * conforme exemplo.
+	 */
+	
 	public static String getTotal(String line) {
 		ArrayList<String> produto = new ArrayList<>();
 		String listProduto[] = line.split(",");
