9aa1d8917348b66e878985b78a489d89ef2a3314
kayanerocha <kayannerch@gmail.com>
2024-01-25 21:27:54 -0300

    curingas delimitados covariancia e contravariancia

---
+++ Secao19/aula241/application/Aula241Dois.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Aula241Dois {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		/* 
+		 * source: lista de covariância, pode ser acessada
+		 * destiny lista de contravriância, pode inserir dados
+		 */
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
+
+}
