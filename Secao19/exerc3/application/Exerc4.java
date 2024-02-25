3ec59911251fdb8f9458fb2a0b43b3612d3aea73
kayanerocha <kayannerch@gmail.com>
2024-02-25 18:02:44 -0300

    exercício com map e linkedhashmap

--- Secao19/exerc3/application/Exerc4.java
+++ Secao19/exerc3/application/Exerc4.java
@@ -3,11 +3,9 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Exerc4 {
 
